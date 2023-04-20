04ceec690cb721c82b1cc88054a7c69868898a40
TheLion10 <itzhak.galperin@mail.huji.ac.il>
2023-04-20 15:00:53 +0300

    Did a lot of stuff

---
+++ Ex2_Tag/Assets/Scripts/PlayerMoverInput.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoverInput : NetworkBehaviour
+{
+    CharacterController character;
+
+    private Vector3 moveVector;
+
+    private float speed = 1f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!IsOwner) return;
+        if (moveVector.Equals(Vector3.zero))
+        {
+            GetComponent<Animator>().SetBool("Is Walking", false);
+            GetComponent<Animator>().SetBool("Is Running", false);
+        }
+        else if (!GetComponent<Animator>().GetBool("Is Walking") && !GetComponent<Animator>().GetBool("Is Running"))
+        {
+            GetComponent<Animator>().SetBool("Is Walking", true);
+            speed = 1f;
+            Invoke("StartRunning", 1);
+        }
+        
+        if (moveVector.z != 0)
+        {
+            character.Move(transform.forward * speed * Time.fixedDeltaTime * Math.Sign(moveVector.z));
+        }
+        
+        if (moveVector.x != 0)
+        {
+            Quaternion rotation = Quaternion.Euler(0, transform.eulerAngles.y + 90 * Math.Sign(moveVector.x), 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.03f);
+        }
+    }
+
+    public void OnMovementChanged(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+        moveVector = new Vector3(direction.x, 0, direction.y);
+        moveVector.Normalize();
+    }
+
+    void StartRunning()
+    {
+        if (!GetComponent<Animator>().GetBool("Is Walking"))
+        {
+            return;
+        }
+        GetComponent<Animator>().SetBool("Is Running", true);
+        GetComponent<Animator>().SetBool("Is Walking", false);
+        speed = 3f;
+    }
+}
