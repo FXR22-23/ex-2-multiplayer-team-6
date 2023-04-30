0c34375dc810befd08c5367f9aa32e33e28e89be
TheLion10 <itzhak.galperin@mail.huji.ac.il>
2023-04-30 13:35:39 +0300

    Finished Task

--- MultiplayerVR - old/Assets/Scripts/MenuHandler.cs
+++ MultiplayerVR - old/Assets/Scripts/MenuHandler.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
+    private bool isClient = false;
+    private bool initialized = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (!initialized && SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (isClient)
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+            else
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+
+            initialized = true;
+        }
+        
+    }
+
     public void OnClickHost()
     {
-        NetworkManager.Singleton.StartHost();
-        Debug.Log("Yayyyyy hosttttt");
+        SceneManager.LoadScene(1);
     }
     public void OnClickClient()
     {
-        NetworkManager.Singleton.StartClient();
-        Debug.Log("Yayyyyy clienttttttt");
+        isClient = true;
+        SceneManager.LoadScene(1);
     }
 }
